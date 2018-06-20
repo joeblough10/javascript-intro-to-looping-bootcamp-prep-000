2caebe140c2c1c58a2528f282f665738a3a64200
alfie8000@gmail.com <alfie8000@gmail.com>
2018-06-20 21:08:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,5 +22,11 @@ while (countdown > 0) {
 }
 
 function doWhileLoop(array) {
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
 }
